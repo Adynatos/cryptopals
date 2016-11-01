0eedd019f0d2b2561b334824842582714074dce6
Kuba Pawlaczyk <pawlaczyk.jakub@gmail.com>
2016-11-01 14:52:23 +0100

    Moved tests for base64 into separate file

---
+++ libcrypto/helpers.cpp
@@ -0,0 +1,86 @@
+#include "helpers.hpp"
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+
+static const std::string base64_chars =
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "0123456789+/";
+
+std::string hexToByte(const std::string& hexString)
+{
+    std::string bytes;
+    for(unsigned int i = 0; i < hexString.length(); i += 2)
+    {
+        std::string byteString = hexString.substr(i, 2);
+        char byte = (char) strtol(byteString.c_str(), NULL, 16);
+        bytes.push_back(byte);
+    }
+    return bytes;
+}
+
+std::string byteToHex(const std::string& byteString)
+{
+    std::stringstream ss;
+    ss << std::hex;
+    for(const auto& byte : byteString)
+        ss << std::setw(2) << std::setfill('0') << (int)byte;
+    return ss.str();
+}
+
+std::string fixedXor(std::string first, const std::string& second)
+{
+    for(unsigned int i = 0; i < first.size(); ++i)
+        first[i] ^= second[i];
+    return first;
+}
+
+std::string base64Encode(const std::string& byteString)
+{
+    std::string result;
+    unsigned int neededPadding = 0;
+    for(unsigned int i = 0; i < byteString.size(); i += 3)
+    {
+        auto chunk = byteString.substr(i, 3);
+        if(chunk.size() != 3)
+        {
+            neededPadding = 3 - chunk.size();
+            chunk += std::string("\0", neededPadding) ;
+        }
+        result += base64_chars[(chunk[0] & 0xFC) >> 2];
+        result += base64_chars[((chunk[0] & 0x03) << 4) + ((chunk[1] & 0xF0) >> 4)];
+        result += base64_chars[((chunk[1] & 0x0F) << 2) + ((chunk[2] & 0xC0) >> 6)];
+        result += base64_chars[chunk[2] & 0x3F];
+    }
+
+    if(byteString.size() % 3)
+    {
+        for(unsigned int i = 0; i < neededPadding; ++i)
+        {
+            result[result.size()-1-i] = '=';
+        }
+    }
+
+    return result;
+}
+
+std::string base64Decode(const std::string& encoded)
+{
+    std::string result;
+    auto paddingSize = std::count(std::begin(encoded), std::end(encoded), '=');
+
+    for(unsigned int i = 0; i < encoded.size(); i += 4)
+    {
+        auto chunk = encoded.substr(i, 4);
+
+        result += (base64_chars.find(chunk[0]) << 2) +((base64_chars.find(chunk[1]) & 0x30) >> 4);
+        result += ((base64_chars.find(chunk[1]) & 0x0F) << 4) + ((base64_chars.find(chunk[2]) & 0x3C) >> 2);
+        result += ((base64_chars.find(chunk[2]) & 0x03) << 6) + base64_chars.find(chunk[3]);
+    }
+
+    auto sizeWithoutPadding = result.size() - paddingSize;
+    result.erase(std::begin(result) + sizeWithoutPadding, std::end(result));
+
+    return result;
+}
--- test/testcrypto/testhelpers.cpp
+++ test/testcrypto/testhelpers.cpp
@@ -36,39 +36,3 @@ TEST(TestHelpers, ShouldXorByteStrings)
     std::string result = "746865206b696420646f6e277420706c6179";
     ASSERT_EQ(result, byteToHex(fixedXor(first, second)));
 }
-
-TEST(TestBase64, ShouldEncodeEmptyString)
-{
-    ASSERT_EQ("", base64Encode(""));
-}
-
-TEST(TestBase64, ShouldEncodeByteString)
-{
-    std::string input = "I'm killing your brain like a poisonous mushroom";
-    std::string result = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-    ASSERT_EQ(result, base64Encode(input));
-}
-
-TEST(TestBase64, ShouldEncodeWithPadding)
-{
-    ASSERT_EQ("VW5kZXIgVGhlIFN1bg==", base64Encode("Under The Sun"));
-    ASSERT_EQ("VGhlIE5vcnRoIFdpbmQ=", base64Encode("The North Wind"));
-}
-
-TEST(TestBase64, ShouldDecodeEmptyString)
-{
-    ASSERT_EQ("", base64Decode(""));
-}
-
-TEST(TestBase64, ShouldDecodeBase64String)
-{
-    std::string input = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-    std::string result = "I'm killing your brain like a poisonous mushroom";
-    ASSERT_EQ(result, base64Decode(input));
-}
-
-TEST(TestBase64, ShouldDecodeWithPadding)
-{
-    ASSERT_EQ("Under The Sun", base64Decode("VW5kZXIgVGhlIFN1bg=="));
-    ASSERT_EQ("The North Wind", base64Decode("VGhlIE5vcnRoIFdpbmQ="));
-}
